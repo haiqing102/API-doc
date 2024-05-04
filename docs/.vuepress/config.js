c21e020f11123dc4f4e45da06e128317fe2d3ede
haiqing <363563768@qq.com>
2024-05-05 01:13:59 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,13 +20,13 @@ module.exports = {
     nav,
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: "/img/gif/logo.gif", // 导航栏logo
-    // repo: "qimu666/api-sdk", // 导航栏右侧生成Github链接
+    repo: "haiqing102/API-sdk", // 导航栏右侧生成Github链接
     searchMaxSuggestions: 10, // 搜索结果显示最大数
     lastUpdated: "上次更新", // 更新的时间，及前缀文字   string | boolean (取值为git提交时间)
 
     docsDir: "docs", // 编辑的文件夹
     editLinks: true, // 编辑链接
-    // docsRepo: "qimu666/api-doc",
+    docsRepo: "haiqing102/API-sdk",
     editLinkText: "帮助我们改善此页面！",
 
     // 以下配置是Vdoing主题改动的和新增的配置
